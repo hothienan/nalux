37a3a90b12d70a045d37acf857f1a90a4457e8b7
congtampham <tampc@forixwebdesign.com>
2023-04-18 11:08:42 +0700

    UPdate Project

--- wp-content/themes/nalux/page-project.php
+++ wp-content/themes/nalux/page-project.php
@@ -18,70 +18,81 @@ $lang = pll_current_language('slug') == 'en' ? 'en' : 'vn';
             <div class="project-inner">
                 <div class="items">
                     <?php
-                    the_content();
                     $termArg = array();
-                    $terms = get_terms( array(
+                    $terms = get_terms(array(
                         'taxonomy' => 'project-group', // set your taxonomy here
                         'hide_empty' => true)); // default: true
-                    if ( !empty( $terms ) && !is_wp_error( $terms ) ) {
+                    if (!empty($terms) && !is_wp_error($terms)) {
                         $items = 1;
-                        foreach( $terms as $term ) {
-                            $img  = get_field( 'project-group-image', $term );
-                            $link = esc_url( get_term_link( $term ) );
+                        foreach ($terms as $term) {
+                            $img = get_field('project-group-image', $term);
+                            $link = esc_url(get_term_link($term));
                             $name = $term->name;
+                            $tname = explode(' ', $name);
+                            foreach ($tname as $i => $n) {
+                                if (trim(strtoupper($n)) == "BĐS") {
+                                    $tname[$i] = nl2br("BĐS \n");
+                                }
+                                if (strtoupper($n) == "PROPERTY") {
+                                    $tname[$i] = nl2br("\n Property");
+                                }
+                            }
+                            $name = implode(' ', $tname);
                             $termArg[] = array($img, $link, $name);
                         }
-                        ?>
-<!--                        <div class="item col-01">-->
-<!--                            <div class="page-title">-->
-<!--                                <h1 class="heading-title">--><?php //echo @__pll('Project'); ?><!--</h1>-->
-<!--                            </div>-->
-<!--                            <a href="project-list.html" class="project-item gradient-top">-->
-<!--                                <div class="photo">-->
-<!--                                    <img src="images/project-photo-01.png" alt="">-->
-<!--                                </div>-->
-<!--                                <div class="project-name">-->
-<!--                                    BĐS<br>nghỉ dưỡng-->
-<!--                                </div>-->
-<!--                            </a>-->
-<!--                        </div>-->
-<!--                        <div class="item col-02">-->
-<!--                            <a href="project-list.html" class="project-item  gradient-bottom">-->
-<!--                                <div class="photo">-->
-<!--                                    <img src="images/project-photo-01.png" alt="">-->
-<!--                                </div>-->
-<!--                                <div class="project-name">-->
-<!--                                    BĐS<br>đô thị-->
-<!--                                </div>-->
-<!--                            </a>-->
-<!--                            <a href="project-list.html" class="project-item  gradient-top">-->
-<!--                                <div class="photo">-->
-<!--                                    <img src="images/project-photo-01.png" alt="">-->
-<!--                                </div>-->
-<!--                                <div class="project-name">-->
-<!--                                    BĐS<br>công nghiệp-->
-<!--                                </div>-->
-<!--                            </a>-->
-<!--                        </div>-->
-<!--                        <div class="item col-03">-->
-<!--                            <a href="project-list.html" class="project-item  gradient-top">-->
-<!--                                <div class="photo">-->
-<!--                                    <img src="images/project-photo-01.png" alt="">-->
-<!--                                </div>-->
-<!--                                <div class="project-name">-->
-<!--                                    BĐS<br>cho thuê-->
-<!--                                </div>-->
-<!--                            </a>-->
-<!--                            <a href="project-list.html" class="project-item  gradient-bottom">-->
-<!--                                <div class="photo">-->
-<!--                                    <img src="images/project-photo-01.png" alt="">-->
-<!--                                </div>-->
-<!--                                <div class="project-name">-->
-<!--                                    Thương mại-->
-<!--                                </div>-->
-<!--                            </a>-->
-<!--                        </div>-->
+                    }
+                    ?>
                     <?php
+                    $i = 1;
+                    $count = ceil((count($termArg) - 1) / 2);
+                    if (count($termArg) >= 1) {
+                        ?>
+                        <div class="item col-01">
+                            <div class="page-title">
+                                <h1 class="heading-title"><?php echo @pll__("Projects"); ?></h1>
+                            </div>
+                            <a href="<?php echo $termArg[0][1]; ?>" class="project-item gradient-top">
+                                <div class="photo">
+                                    <img src="<?php echo $termArg[0][0]; ?>" alt="<?php echo $termArg[0][2]; ?>">
+                                </div>
+                                <div class="project-name">
+                                    <?php
+                                    echo $termArg[0][2];
+                                    ?>
+                                </div>
+                            </a>
+                        </div>
+                        <?php
+                    }
+                    if ($count >= 1) {
+                        for ($ii = 1; $ii <= $count; $ii++) {
+                            ?>
+                            <div class="item col-02">
+                                <a href="<?php echo $termArg[$ii + ($ii - 1)][1]; ?>" class="project-item gradient-top">
+                                    <div class="photo">
+                                        <img src="<?php echo $termArg[$ii + ($ii - 1)][0]; ?>"
+                                             alt="<?php echo $termArg[$ii + ($ii - 1)][2]; ?>">
+                                    </div>
+                                    <div class="project-name">
+                                        <?php
+                                        echo $termArg[$ii + ($ii - 1)][2];
+                                        ?>
+                                    </div>
+                                </a>
+                                <a href="<?php echo $termArg[$ii + ($ii)][1]; ?>" class="project-item gradient-top">
+                                    <div class="photo">
+                                        <img src="<?php echo $termArg[$ii + ($ii)][0]; ?>"
+                                             alt="<?php echo $termArg[$ii + ($ii)][2]; ?>">
+                                    </div>
+                                    <div class="project-name">
+                                        <?php
+                                        echo $termArg[$ii + ($ii)][2];
+                                        ?>
+                                    </div>
+                                </a>
+                            </div>
+                            <?php
+                        }
                     }
                     ?>
                 </div>
